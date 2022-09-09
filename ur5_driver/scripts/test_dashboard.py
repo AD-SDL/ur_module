0ccdc21cd6d3eca3836310f9940be806c2f72fc5
Kendrick Xie <kendrickxie@gmail.com>
2022-09-09 15:21:02 -0500

    add script to check ur5 program state through dashboard

---
+++ ur5_driver/scripts/test_dashboard.py
@@ -0,0 +1,14 @@
+import socket
+import time
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(2)
+s.connect(('192.168.1.102', 29999))
+def sendCommand(cmd):
+ cmd = cmd + '\n'
+ print(cmd)
+ s.sendall(cmd.encode())
+ time.sleep(5)
+ rcvd = s.recv(4096)
+ print(rcvd)
+while (1):
+ sendCommand('programState')
