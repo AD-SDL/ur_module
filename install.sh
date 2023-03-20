3a0a6832d3d718fcbbdbaef594500977bdf23404
Dozgulbas <dozgulbas@anl.gov>
2023-03-20 16:03:21 -0500

    Working on python-urx installation steps

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 
-sudo apt install python3-colcon-common-extensions python3-vcstool
-export COLCON_WS=/ros_ur_driver
-mkdir -p $COLCON_WS/src
-cd $COLCON_WS
-git clone https://github.com/UniversalRobots/Universal_Robots_ROS2_Driver.git src/Universal_Robots_ROS2_Driver
-vcs import src --skip-existing --input src/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver-not-released.${ROS_DISTRO}.repos
-rosdep update
-rosdep install --ignore-src --from-paths src -y -r
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
-source install/setup.bash
+# sudo apt install python3-colcon-common-extensions python3-vcstool
+# rosdep update
+# rosdep install --ignore-src --from-paths src -y -r
+# colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+# source install/setup.bash
+git clone https://github.com/SintefManufacturing/python-urx.git
+cd python-urx
+pip install .
