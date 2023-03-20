6f0cb10040d3df7c885831ea7c27b933ddf82c78
Dozgulbas <dozgulbas@anl.gov>
2023-03-20 16:52:09 -0500

    Adding installing henry module steps

--- install_hendry_module.sh
+++ install_hendry_module.sh
@@ -1,9 +1,9 @@
-
 sudo apt install python3-colcon-common-extensions python3-vcstool
+export COLCON_WS=/ur5_ws
+mkdir -p $COLCON_WS/src
+cd $COLCON_WS
+git clone https://github.com/AD-SDL/henry_module.git
 rosdep update
 rosdep install --ignore-src --from-paths src -y -r
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
 source install/setup.bash
-git clone https://github.com/SintefManufacturing/python-urx.git
-cd python-urx
-pip install .
