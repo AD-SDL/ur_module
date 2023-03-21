796cbe725b5b1f7da1a72bf5585972a0003d35b4
Dozgulbas <dozgulbas@anl.gov>
2023-03-21 16:07:02 -0500

    Working on the UR client

--- ur5_client/ur5_client/ur5_client.py
+++ ur5_client/ur5_client/ur5_client.py
@@ -1,45 +1,41 @@
 #! /usr/bin/env python3
 
-import rclpy                 # import Rospy
+import rclpy  # import Rospy
 from rclpy.node import Node  # import Rospy Node
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
 from std_msgs.msg import String
-from std_srvs.srv import Empty
 
 from ur5_driver.ur5_driver import UR5 
 from time import sleep
 
-
 from wei_services.srv import WeiDescription 
 from wei_services.srv import WeiActions  
 
-class UR5ClientNode(Node):
+class UR5Client(Node):
     '''
     The jointControlNode inputs data from the 'action' topic, providing a set of commands for the driver to execute. It then receives feedback, 
     based on the executed command and publishes the state of the peeler and a description of the peeler to the respective topics.
     '''
-    def __init__(self, NODE_NAME = "UR5_Client_Node"):
+    def __init__(self, TEMP_NODE_NAME = "UR5_Client_Node"):
         '''
         The init function is neccesary for the peelerNode class to initialize all variables, parameters, and other functions.
         Inside the function the parameters exist, and calls to other functions and services are made so they can be executed in main.
         '''
 
-        super().__init__(NODE_NAME)
-        
-        print("UR5 is online") 
+        super().__init__(TEMP_NODE_NAME)
+        self.node_name = self.get_name()
 
         self.state = "UNKNOWN"
-        # self.ur5 = UR5()
-        # self.ur5.initialize_robot()
 
         timer_period = 0.5  # seconds
         self.stateTimer = self.create_timer(timer_period, self.stateCallback)
 
-        self.statePub = self.create_publisher(String, NODE_NAME + '/state', 10)
+        self.statePub = self.create_publisher(String, self.node_name + '/state', 10)
 
         self.stateTimer = self.create_timer(timer_period, self.stateCallback)
-
    
-        self.action_handler = self.create_service(WeiActions, NODE_NAME + "/action_handler", self.actionCallback)
+        self.action_handler = self.create_service(WeiActions, self.node_name + "/action_handler", self.actionCallback)
 
         self.description={}
 
@@ -131,9 +127,26 @@ def main(args = None):
 
     NAME = "UR5_Client_Node"
     rclpy.init(args = args)  # initialize Ros2 communication
-    node = UR5ClientNode(NODE_NAME = NAME)
+    node = UR5Client(NODE_NAME = NAME)
     rclpy.spin(node)     # keep Ros2 communication open for action node
     rclpy.shutdown()     # kill Ros2 communication
+    rclpy.init(args=args)       # initialize Ros2 communication
+
+    try:
+        peeler_client = UR5Client()
+        executor = MultiThreadedExecutor()
+        executor.add_node(peeler_client)
+
+        try:
+            peeler_client.get_logger().info('Beginning client, shut down with CTRL-C')
+            executor.spin()
+        except KeyboardInterrupt:
+            peeler_client.get_logger().info('Keyboard interrupt, shutting down.\n')
+        finally:
+            executor.shutdown()
+            peeler_client.destroy_node()
+    finally:
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
