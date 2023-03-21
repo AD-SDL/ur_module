865167b3a02ce5190f5a354d8d8f00053ed69292
Dozgulbas <dozgulbas@anl.gov>
2023-03-21 18:18:15 -0500

    Added get robot movement state function

--- ur5_driver/ur5_driver/ur5_driver.py
+++ ur5_driver/ur5_driver/ur5_driver.py
@@ -6,9 +6,9 @@ from multiprocessing.connection import wait
 from time import sleep
 from copy import deepcopy
 
-from ur_dashboard import UR_DASHBOARD
-import robotiq_gripper as robotiq_gripper
-from urx import Robot
+from ur5_driver.ur_dashboard import UR_DASHBOARD
+import ur5_driver.robotiq_gripper as robotiq_gripper
+from urx import Robot, RobotException
 
 class UR5(UR_DASHBOARD):
     
@@ -26,6 +26,7 @@ class UR5(UR_DASHBOARD):
         self.acceleration = 1.0
         self.velocity = 0.5
 
+        self.robot_current_joint_angles = self.get_joint_angles()
         self.module_entry = [-0.1828145484680406, 0.1501917529215074, 0.4157045667286946, -0.014753354925067616, -3.133785224432585, -0.01020982277167234]
         self.module_entry_joint = [-1.3963525930987757, -2.1945158443846644, 2.1684568564044397, -1.5495260164937754, -1.5337546507464808, 3.2634336948394775]
         self.home = [-0.13358071546889347, -0.009673715752021885, 0.5890782758304143, -0.014566051910791617, -3.133734935087693, -0.010359747956377084]
@@ -65,8 +66,8 @@ class UR5(UR_DASHBOARD):
                 self.ur5 = Robot(self.IP)
                 sleep(2)
 
-            except Exception as err:
-                print(err)
+            except Exception:
+                print("Trying robot connection ...")
             else:
                 print('Successful ur5 connection')
                 break
@@ -77,6 +78,22 @@ class UR5(UR_DASHBOARD):
         """
         self.ur5.close()
         print("Robot connection is closed.")
+
+    def get_joint_angles(self):
+        
+        return self.ur5.getj()
+
+    def get_movement_state(self):
+        current_location = self.get_joint_angles()
+        
+        if self.robot_current_joint_angles == current_location:
+            movement_state = "READY"
+        else:
+            movement_state = "BUSY"
+
+        self.robot_current_joint_angles = current_location
+
+        return movement_state
 
     def pick(self, pick_goal):
 
@@ -167,5 +184,8 @@ if __name__ == "__main__":
     # robot.ur5.set_payload(2, (0, 0, 0.1))
     sleep(0.2)  #leave some time to robot to process the setup commands    # robot.transfer(pos2,pos1)
     # print(robot.ur5.getl())
+    for i in range(10):
+        print(robot.get_movement_state())
+        sleep(0.5)
     robot.disconnect_ur()
     print('end')
