0e34c880d300df1caf795dd605cb24fec7edd0c4
Dozgulbas <dozgulbas@anl.gov>
2023-05-26 16:49:27 -0500

    Updated the driver

--- ur5_driver/ur5_driver/ur5_driver.py
+++ ur5_driver/ur5_driver/ur5_driver.py
@@ -14,16 +14,42 @@ from urx import Robot, RobotException
 class UR5(UR_DASHBOARD):
     
 
-    def __init__(self, IP:str = "146.137.240.38", PORT: int = 29999):
+    def __init__(self, IP:str = "146.137.240.38", PORT: int = 29999, gripper:bool = False, tool_changer_pv:str = None, pipette_pv:str = None, camera_pv:str = None):
         
         super().__init__(IP=IP, PORT=PORT)
 
-        # ur5 SETUP:
-        self.ur5 = None
+        # ur SETUP:
+        self.ur = None
+        self.gripper = None
+        self.pipette = None
+        self.tool_changer = None
+        self.camera = None
+    
         self.connect_ur()
+        if gripper:
+            self.connect_gripper()
+        if tool_changer_pv:
+            self.connect_tool_changer(tool_changer_pv)
+        if pipette_pv:
+            self.connect_pipette(pipette_pv)
+        if camera_pv:
+            self.connect_camera(camera_pv)
 
-        self.acceleration = 2
-        self.velocity = 2
+        self.pipette_drop_tip_value = -8
+        self.pipette_aspirate_value = 2.0
+        self.pipette_dispense_value = -2.0
+        self.droplet_value = 0.3
+
+        self.gripper_close = 130 # 0-255 (255 is closed)
+        self.griper_open = 0
+        self.gripper_speed = 150 # 0-255
+        self.gripper_force = 0 # 0-255
+
+        print('Opening gripper...')
+        self.gripper.move_and_wait_for_pos(self.griper_open, self.gripper_speed, self.gripper_force)
+
+        self.acceleration = 0.5
+        self.velocity = 0.5
         self.robot_current_joint_angles = None
 
         self.get_movement_state()
@@ -35,27 +61,6 @@ class UR5(UR_DASHBOARD):
         self.plate_exchange_1_above = [-0.18284724105645211, 0.7914820291585895, 0.41175512257988434, -0.014545475433050672, -3.1337759450718, -0.010278634391729295]
         self.plate_exchange_1 = [-0.1828537989205587, 0.7914917511283945, 0.390542100409092, -0.014571172649734884, -3.133719848650817, -0.010138239501312422]
 
-        # GRIPPER SETUP:
-        print('Creating gripper...')
-        self.gripper = robotiq_gripper.RobotiqGripper()
-        print('Connecting to gripper...')
-        
-        self.gripper.connect(self.IP, 63352)
-
-        if self.gripper.is_active():
-            print('Gripper already active')
-        else:
-            print('Activating gripper...')
-            self.gripper.activate()
-        
-
-        self.gripper_close = 130 # 0-255 (255 is closed)
-        self.griper_open = 0
-        self.gripper_speed = 150 # 0-255
-        self.gripper_force = 0 # 0-255
-
-        print('Opening gripper...')
-        self.gripper.move_and_wait_for_pos(self.griper_open, self.gripper_speed, self.gripper_force)
 
     def connect_ur(self):
         """
@@ -64,29 +69,96 @@ class UR5(UR_DASHBOARD):
 
         for i in range(10):
             try:
-                self.ur5 = Robot(self.IP)
+                self.ur = Robot(self.IP)
                 sleep(2)
 
             except socket.error:
                 print("Trying robot connection ...")
             else:
-                print('Successful ur5 connection')
+                print('Successful ur connection')
                 break
+
+    def connect_gripper(self):
+        """
+        Connect to the gripper
+        """
+        try:
+            # GRIPPER SETUP:
+            self.gripper = robotiq_gripper.RobotiqGripper()
+            print('Connecting to gripper...')
+            self.gripper.connect(self.IP, 63352)
+
+        except Exception as err:
+            print("Gripper error: ", err)
+
+        else:
+            if self.gripper.is_active():
+                print('Gripper already active')
+            else:
+                print('Activating gripper...')
+                self.gripper.activate()
+
+    def connect_tool_changer(self, tool_changer_pv:str):
+        """
+        Connect tool changer
+        """
+
+        try:
+            # Establishing a connection with the tool changer using EPICS library.
+            self.tool_changer = epics.PV(tool_changer_pv)
+
+        except Exception as err:
+            print("Tool changer error: ", err)
+
+        else:
+            print("Tool changer is connected.")
+
+    def connect_pipette(self, pipette_pv:str):
+        """
+        Connect pipette
+        """
+
+        try:
+            # Establishing a connection with the pipette using EPICS library.
+            self.pipette = epics.PV(pipette_pv)
+
+        except Exception as err:
+            print("Pipette error: ", err)
+
+        else:
+            print("Pipette is connected.")
+
+    def connect_camera(self, camera_pv:str):
+        """
+        Connect camera
+        """
+
+        try:
+            # Establishing a connection with the camera using EPICS library.
+            self.camera =  epics.PV("8idiARV1:cam1:Acquire")
+            self.cam_image = epics.PV("8idiARV1:Pva1:Image")
+            self.cam_capture =  epics.PV("8idiARV1:Pva1:Capture")
+
+        except Exception as err:
+            print("Pipette error: ", err)
+
+        else:
+            print("Pipette is connected.")
 
     def disconnect_ur(self):
         """
         Description: Disconnects the socket connection with the UR robot
         """
-        self.ur5.close()
+        self.ur.close()
         print("Robot connection is closed.")
 
     def get_joint_angles(self):
         
-        return self.ur5.getj()
+        return self.ur.getj()
     
     def get_cartesian_coordinates(self):
         
-        return self.ur5.getl()
+        return self.ur.getl()
     
     def get_movement_state(self):
         current_location = self.get_joint_angles()
@@ -101,6 +173,17 @@ class UR5(UR_DASHBOARD):
 
         return movement_state
 
+    def home(self):
+        """
+        Description: Moves the robot to the home location.
+        """
+        print("Homing the robot...")
+        self.ur.movej(self.home_joint, self.acceleration, self.velocity, 0, 0)
+        sleep(4)
+
+        print("Robot moved to home location")
+
+
     def pick(self, pick_goal):
 
         '''Pick up from first goal position'''
@@ -109,32 +192,32 @@ class UR5(UR_DASHBOARD):
         above_goal[2] += 0.05
 
         print('Moving to home position')
-        # self.ur5.movel(self.home, self.acceleration, self.velocity)
-        self.ur5.movej(self.home_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.home, self.acceleration, self.velocity)
+        self.ur.movej(self.home_joint, self.acceleration, self.velocity)
 
         print("Moving to the module entry location")
-        # self.ur5.movel(self.module_entry, self.acceleration, self.velocity)
-        self.ur5.movej(self.module_entry_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.module_entry, self.acceleration, self.velocity)
+        self.ur.movej(self.module_entry_joint, self.acceleration, self.velocity)
 
         print('Moving to above goal position')
-        self.ur5.movel(above_goal, self.acceleration, self.velocity)
+        self.ur.movel(above_goal, self.acceleration, self.velocity)
 
         print('Moving to goal position')
-        self.ur5.movel(pick_goal, self.acceleration, self.velocity)
+        self.ur.movel(pick_goal, self.acceleration, self.velocity)
 
         print('Closing gripper')
         self.gripper.move_and_wait_for_pos(self.gripper_close, self.gripper_speed, self.gripper_force)
 
         print('Moving back to above goal position')
-        self.ur5.movel(above_goal, self.acceleration, self.velocity)
+        self.ur.movel(above_goal, self.acceleration, self.velocity)
 
         print("Moving to the module entry location")
-        # self.ur5.movel(self.module_entry, self.acceleration, self.velocity)
-        self.ur5.movej(self.module_entry_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.module_entry, self.acceleration, self.velocity)
+        self.ur.movej(self.module_entry_joint, self.acceleration, self.velocity)
 
         print('Moving to home position')
-        # self.ur5.movel(self.home, self.acceleration, self.velocity)
-        self.ur5.movej(self.home_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.home, self.acceleration, self.velocity)
+        self.ur.movej(self.home_joint, self.acceleration, self.velocity)
 
 
     def place(self, place_goal):
@@ -145,42 +228,67 @@ class UR5(UR_DASHBOARD):
         above_goal[2] += 0.05
 
         print('Moving to home position')
-        # self.ur5.movel(self.home, self.acceleration, self.velocity)
-        self.ur5.movej(self.home_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.home, self.acceleration, self.velocity)
+        self.ur.movej(self.home_joint, self.acceleration, self.velocity)
 
         print("Moving to the module entry location")
-        # self.ur5.movel(self.module_entry, self.acceleration, self.velocity)
-        self.ur5.movej(self.module_entry_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.module_entry, self.acceleration, self.velocity)
+        self.ur.movej(self.module_entry_joint, self.acceleration, self.velocity)
 
         print('Moving to above goal position')
-        self.ur5.movel(above_goal, self.acceleration, self.velocity)
+        self.ur.movel(above_goal, self.acceleration, self.velocity)
 
         print('Moving to goal position')
-        self.ur5.movel(place_goal, self.acceleration, self.velocity)
+        self.ur.movel(place_goal, self.acceleration, self.velocity)
 
         print('Opennig gripper')
         self.gripper.move_and_wait_for_pos(self.griper_open, self.gripper_speed, self.gripper_force)
 
         print('Moving back to above goal position')
-        self.ur5.movel(above_goal, self.acceleration, self.velocity)
+        self.ur.movel(above_goal, self.acceleration, self.velocity)
 
         print("Moving to the module entry location")
-        # self.ur5.movel(self.module_entry, self.acceleration, self.velocity)
-        self.ur5.movej(self.module_entry_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.module_entry, self.acceleration, self.velocity)
+        self.ur.movej(self.module_entry_joint, self.acceleration, self.velocity)
 
         print('Moving to home position')
-        # self.ur5.movel(self.home, self.acceleration, self.velocity)
-        self.ur5.movej(self.home_joint, self.acceleration, self.velocity)
+        # self.ur.movel(self.home, self.acceleration, self.velocity)
+        self.ur.movej(self.home_joint, self.acceleration, self.velocity)
 
         
-    def transfer(self, pos1, pos2):
+    def transfer(self, pos1, pos2, gripper_rotation:str = None, safe_heigh: int = None):
         ''''''
-        self.ur5.set_tcp((0, 0, 0, 0, 0, 0))
-        # robot.ur5.set_payload(2, (0, 0, 0.1))
+        self.ur.set_tcp((0, 0, 0, 0, 0, 0))
+        # robot.ur.set_payload(2, (0, 0, 0.1))
 
         self.pick(pos1)
         self.place(pos2)
         print('Finished transfer')
+
+    def run_urp_program(self, transfer_file_path:str = None, program_name: str = None):
+
+        """"""
+        if not program_name:
+            print("Provide program name!")
+            return
+        
+        ur_program_path = "/programs/program_name"
+
+        if transfer_file_path:
+            self.transfer_program(local_path = transfer_file_path, ur_path = ur_program_path)
+            sleep(2)
+
+        self.load_program(program_path = ur_program_path)
+        sleep(2)
+        self.run_program()
+        
+        run_status = "Busy"
+        while run_status == "Busy":
+            if self.get_program_run_status() != "running": # TODO: Change this with the correct output message
+                run_status = "Completed"
+
+        return 
+
 
 if __name__ == "__main__":
 
