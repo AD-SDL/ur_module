1a2a96bfd87457b3fc5049d78292a1bf9560a8f9
Dozgulbas <dozgulbas@anl.gov>
2023-05-31 16:13:28 -0500

    Added new launch parameters

--- ur5_client/launch/ur5_client.launch.py
+++ ur5_client/launch/ur5_client.launch.py
@@ -13,8 +13,13 @@ def generate_launch_description():
     launch_d = LaunchDescription()
 
     ip = LaunchConfiguration("ip")
-    tool = LaunchConfiguration("tool")
-    tool_address = LaunchConfiguration("tool_address")
+    gripper = LaunchConfiguration("gripper")
+    vacuum_gripper = LaunchConfiguration("vacuum_gripper")
+    screwdriver = LaunchConfiguration("screwdriver")
+    tool_changer_pv = LaunchConfiguration("tool_changer_pv")
+    pipette_pv = LaunchConfiguration("pipette_pv")
+    camera_pv = LaunchConfiguration("camera_pv")
+
     
     declare_use_ur_ip_cmd = DeclareLaunchArgument(
         name = "ip",
@@ -22,16 +27,39 @@ def generate_launch_description():
         description= "Flag to accept UR IP"
         )
     
-    declare_use_ur_tool_cmd = DeclareLaunchArgument(
-        name = "tool",
-        default_value= "None",
-        description= "Flag to accept UR tool"
+    declare_use_ur_gripper_cmd = DeclareLaunchArgument(
+        name = "gripper",
+        default_value= "False",
+        description= "Flag to accept UR gripper"
         )
     
-    declare_use_ur_tool_address_cmd = DeclareLaunchArgument(
-        name = "tool_address",
-        default_value= "None",
+    declare_use_ur_vacuum_gripper_cmd = DeclareLaunchArgument(
+        name = "vacuum_gripper",
+        default_value= "False",
+        description= "Flag to accept UR vacuum gripper"
+        )
+    
+    declare_use_ur_screwdriver_cmd = DeclareLaunchArgument(
+        name = "screwdriver",
+        default_value= "False",
+        description= "Flag to accept UR screwdriver"
+        )
+    declare_use_ur_tool_changer_pv_cmd = DeclareLaunchArgument(
+        name = "tool_changer_pv",
+        default_value= "",
         description= "Flag to accept UR tool address"
+        )
+    
+    declare_use_ur_pipette_pv_cmd = DeclareLaunchArgument(
+        name = "pipette_pv",
+        default_value= "",
+        description= "Flag to accept UR pipette_pv"
+        )
+    
+    declare_use_ur_camera_pv_cmd = DeclareLaunchArgument(
+        name = "camera_pv",
+        default_value= "",
+        description= "Flag to accept UR camera_pv"
         )
     
     ur5_client = Node(
@@ -40,13 +68,22 @@ def generate_launch_description():
             executable = 'ur5_client',
             output = "screen",
             name='UR5_Client_Node',
-            parameters = [{"ip":ip},{"tool":tool},{"tool_address":tool_address}],
+            parameters = [{"ip":ip},
+                          {"gripper":gripper},
+                          {"vacuum_gripper":vacuum_gripper},
+                          {"screwdriver":screwdriver},
+                          {"tool_changer_pv":tool_changer_pv},
+                          {"pipette_pv":pipette_pv},
+                          {"camera_pv":camera_pv}],
             emulate_tty=True
     )
     launch_d.add_action(declare_use_ur_ip_cmd)
-    launch_d.add_action(declare_use_ur_tool_cmd)
-    launch_d.add_action(declare_use_ur_tool_address_cmd)
-
+    launch_d.add_action(declare_use_ur_gripper_cmd)
+    launch_d.add_action(declare_use_ur_vacuum_gripper_cmd)
+    launch_d.add_action(declare_use_ur_screwdriver_cmd)
+    launch_d.add_action(declare_use_ur_tool_changer_pv_cmd)
+    launch_d.add_action(declare_use_ur_pipette_pv_cmd)
+    launch_d.add_action(declare_use_ur_camera_pv_cmd)
     launch_d.add_action(ur5_client)
 
     return launch_d
