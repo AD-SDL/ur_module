37e3b40c481cd45d983b4c658f7531074a5f2cb9
Dozgulbas <dozgulbas@anl.gov>
2023-05-31 15:55:39 -0500

    Adding new launch parameters to accept tool and tool address

--- ur5_client/launch/ur5_client.launch.py
+++ ur5_client/launch/ur5_client.launch.py
@@ -13,12 +13,25 @@ def generate_launch_description():
     launch_d = LaunchDescription()
 
     ip = LaunchConfiguration("ip")
-
+    tool = LaunchConfiguration("tool")
+    tool_address = LaunchConfiguration("tool_address")
     
     declare_use_ur_ip_cmd = DeclareLaunchArgument(
         name = "ip",
         default_value= "146.137.240.38",
         description= "Flag to accept UR IP"
+        )
+    
+    declare_use_ur_tool_cmd = DeclareLaunchArgument(
+        name = "tool",
+        default_value= "None",
+        description= "Flag to accept UR tool"
+        )
+    
+    declare_use_ur_tool_address_cmd = DeclareLaunchArgument(
+        name = "tool_address",
+        default_value= "None",
+        description= "Flag to accept UR tool address"
         )
     
     ur5_client = Node(
@@ -27,10 +40,13 @@ def generate_launch_description():
             executable = 'ur5_client',
             output = "screen",
             name='UR5_Client_Node',
-            parameters = [{"peeler_port":ip}],
+            parameters = [{"ip":ip},{"tool":tool},{"tool_address":tool_address}],
             emulate_tty=True
     )
     launch_d.add_action(declare_use_ur_ip_cmd)
+    launch_d.add_action(declare_use_ur_tool_cmd)
+    launch_d.add_action(declare_use_ur_tool_address_cmd)
+
     launch_d.add_action(ur5_client)
 
     return launch_d
