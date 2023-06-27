7d21c9550e1e595171c4c690f9910cca0fa298c1
Dozgulbas <dozgulbas@anl.gov>
2023-06-27 18:57:47 -0500

    Screwdriver controller update

--- ur_driver/ur_driver/ur_driver.py
+++ ur_driver/ur_driver/ur_driver.py
@@ -7,6 +7,7 @@ import epics
 from multiprocessing.connection import wait
 from time import sleep
 from copy import deepcopy
+import json
 
 from ur_driver.ur_dashboard import UR_DASHBOARD
 import ur_driver.robotiq_gripper as robotiq_gripper
@@ -319,10 +320,19 @@ class VacuumGripperController():
 class ScrewdriverController():
     
 
-    def __init__(self, IP:str = "146.137.240.38", PORT: int = 29999):
-        
-        super().__init__(IP=IP, PORT=PORT)
+    def __init__(self ):
+        """"""
+        self.home_urp = "home_screwdriver.urp"
+        self.drive_forward_urp = "home_screwdriver.urp"
+        self.driver_backward_urp = "home_screwdriver.urp"
 
+    def check_screwdriver_controls(self):
+        pass  
+    def get_urp_programs(self):
+        # Read from file and save it on the ur 
+        pass
+    def update_urp_programs(self):
+        pass
 class PipetteController():
     
 
