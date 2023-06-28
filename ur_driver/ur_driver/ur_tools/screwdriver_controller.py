799899bd6a6563a26b0db0c9165049bba8a7d4a8
Dozgulbas <dozgulbas@anl.gov>
2023-06-28 14:25:13 -0500

    Added screwdriver controller

---
+++ ur_driver/ur_driver/ur_tools/screwdriver_controller.py
@@ -0,0 +1,15 @@
+class ScrewdriverController():
+
+    def __init__(self ):
+        """"""
+        self.home_urp = "home_screwdriver.urp"
+        self.drive_forward_urp = "home_screwdriver.urp"
+        self.driver_backward_urp = "home_screwdriver.urp"
+
+    def check_screwdriver_controls(self):
+        pass  
+    def get_urp_programs(self):
+        # Read from file and save it on the ur 
+        pass
+    def update_urp_programs(self):
+        pass
