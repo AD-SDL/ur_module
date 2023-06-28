dea9285e421639f6f7523576479c87ce3569bcdd
Dozgulbas <dozgulbas@anl.gov>
2023-06-27 21:18:56 -0500

    clean up on pipette controller

--- ur_driver/ur_driver/ur_driver.py
+++ ur_driver/ur_driver/ur_driver.py
@@ -20,28 +20,22 @@ class UR(UR_DASHBOARD):
         
         super().__init__(IP=IP, PORT=PORT)
 
-        # ur SETUP:
         self.ur = None
 
         self.connect_ur()
-        if gripper:
-            self.connect_gripper()
-        if tool_changer_pv:
-            self.connect_tool_changer(tool_changer_pv)
-        if pipette_pv:
-            self.connect_pipette(pipette_pv)
-        if camera_pv:
-            self.connect_camera(camera_pv)
-
-        self.pipette_drop_tip_value = -8
-        self.pipette_aspirate_value = 2.0
-        self.pipette_dispense_value = -2.0
-        self.droplet_value = 0.3
+    
+        self.connect_camera(camera_pv)
 
         self.acceleration = 0.5
-        self.velocity = 0.5
-        self.robot_current_joint_angles = None
+        self.velocity = 0.2
+        self.speed_ms    = 0.750
+        self.speed_rads  = 0.750
+        self.accel_mss   = 1.200
+        self.accel_radss = 1.200
+        self.blend_radius_m = 0.001
+        self.ref_frame = [0,0,0,0,0,0]
 
+        self.robot_current_joint_angles = None
         self.get_movement_state()
 
 
@@ -61,7 +55,6 @@ class UR(UR_DASHBOARD):
             else:
                 print('Successful ur connection')
                 break
-
 
 
     def connect_camera(self, camera_pv:str):
@@ -109,17 +102,88 @@ class UR(UR_DASHBOARD):
 
         return movement_state
 
-    def home(self):
+    def home(self, home_location = None):
         """
         Description: Moves the robot to the home location.
         """
         print("Homing the robot...")
-        self.ur.movej(self.home_joint, self.acceleration, self.velocity, 0, 0)
+        if home_location:
+            home_loc = home_location
+        else:
+            home_loc = [-1.355567757283346, -2.5413090191283167, 1.8447726408587855, -0.891581193809845, -1.5595606009112757, 3.3403327465057373]
+        self.ur.movej(home_loc, self.acceleration, self.velocity, 0, 0)
         sleep(4)
 
         print("Robot moved to home location")
 
+    def pick_pipette(self):
 
+        pipette_controller = PipetteController(ur_connection=self.ur)
+        tool_changer_controller = ToolChangerController()
+
+        pipette_controller.move_pipette_dock()
+        tool_changer_controller.lock_tool_changer()
+        pipette_controller.lift_pipette_on_dock()
+
+    def place_pipette(self):
+
+        pipette_controller = PipetteController(ur_connection=self.ur)
+        tool_changer_controller = ToolChangerController()
+
+        pipette_controller.move_pipette_dock()
+        tool_changer_controller.unlock_tool_changer()
+        pipette_controller.lift_pipette_on_dock()  
+
+    def create_sample(self):
+        """"""
+
+        home_J = [2.017202138900757, -1.137721137409546, -0.9426093101501465, -2.6425615749754847, -4.693090263997213, -3.8424256483661097]
+
+        pipette_controller = PipetteController(ur_connection=self.ur)
+        self.home(home_J)
+        pipette_controller.pick_tip()
+        pipette_controller.make_sample()
+        self.home(home_J)
+
+    def run_droplet(self):
+        pipette_controller = PipetteController(ur_connection=self.ur)
+        pipette_controller.create_droplet()
+        pipette_controller.retrieve_droplet()
+
+    def dispose_tip(self):
+        pipette_controller = PipetteController(ur_connection=self.ur)
+        home_J = [2.017202138900757, -1.137721137409546, -0.9426093101501465, -2.6425615749754847, -4.693090263997213, -3.8424256483661097]
+
+        self.home(home_J)
+        pipette_controller.empty_tip()
+        pipette_controller.drop_tip_to_trash()
+        self.home(home_J)
+        
+    def droplet_exp(self, tip_number_1:int = None, tip_number_2:int = None):
+        """
+        Description: Runs the full droplet experiment by calling the functions that perform each step in the experiment.
+        """
+        print("-*-*-* Starting the droplet experiment *-*-*-")
+        home_J = [2.017202138900757, -1.137721137409546, -0.9426093101501465, -2.6425615749754847, -4.693090263997213, -3.8424256483661097]
+
+        pipette_controller = PipetteController(ur_connection=self.ur)
+        tool_changer_controller = ToolChangerController()
+
+ 
+
+        pipette_controller.move_pipette_dock()
+        tool_changer_controller.lock_tool_changer()
+        pipette_controller.lift_pipette_on_dock()    
+
+        self.home(home_J)
+        pipette_controller.empty_tip()
+        pipette_controller.drop_tip_to_trash()
+        self.home(home_J)
+
+        pipette_controller.move_pipette_dock()
+        tool_changer_controller.unlock_tool_changer()
+        pipette_controller.lift_pipette_on_dock()         
+        print("-*-*-* Droplet experiment is completed *-*-*-")
         
     def gripper_transfer(self, pos1, pos2, gripper_rotation:str = None, safe_heigh: int = None):
         '''
@@ -208,6 +272,15 @@ class GripperController():
         self.griper_open = 0
         self.gripper_speed = 150 # 0-255
         self.gripper_force = 0 # 0-255
+
+        self.acceleration = 0.5
+        self.velocity = 0.2
+        self.speed_ms    = 0.750
+        self.speed_rads  = 0.750
+        self.accel_mss   = 1.200
+        self.accel_radss = 1.200
+        self.blend_radius_m = 0.001
+        self.ref_frame = [0,0,0,0,0,0]
 
         self.module_entry = [-0.1828145484680406, 0.1501917529215074, 0.4157045667286946, -0.014753354925067616, -3.133785224432585, -0.01020982277167234]
         self.module_entry_joint = [-1.3963525930987757, -2.1945158443846644, 2.1684568564044397, -1.5495260164937754, -1.5337546507464808, 3.2634336948394775]
@@ -337,12 +410,46 @@ class PipetteController():
     
 
     def __init__(self, pipette_pv:str = None, ur_connection = None):
+
         self.pv = pipette_pv
         self.ur = ur_connection
+
+        self.acceleration = 0.5
+        self.velocity = 0.2
+        self.speed_ms    = 0.750
+        self.speed_rads  = 0.750
+        self.accel_mss   = 1.200
+        self.accel_radss = 1.200
+        self.blend_radius_m = 0.001
+        self.ref_frame = [0,0,0,0,0,0]
+
         self.pipette_drop_tip_value = -8
         self.pipette_aspirate_value = 2.0
         self.pipette_dispense_value = -2.0
         self.droplet_value = 0.3
+
+        self.pipette_loc = [-0.30710397664568057, 0.2223363316295067, 0.25346649921490616, 0.9780579194931717, -1.3456500374612195, -1.5122814896417478]
+        self.pipette_loc_J = [2.8711442947387695, -1.8251310787596644, -1.5156354904174805, -1.3721376222423096, -4.720762554799215, -3.0886977354632776]
+        self.pipette_approach = [-0.30710468347240427, 0.22234393663902577, 0.2793166289891617, 0.977973715529265, -1.3455795650125528, -1.512392593568845]
+        self.pipette_approach_J = [2.8711442947387695, -1.8102451763548792, -1.412275791168213, -1.4902585309794922, -4.720990244542257, -3.088721577321188]
+        self.pipette_above = [-0.3075719688934094, 0.2227307810713913, 0.40515454739546075, 0.9815940238325527, -1.3416684284127856, -1.504904936327573]
+        self.pipette_above_J = [2.869802236557007, -1.9749981365599574, -0.5613865852355957, -2.1772977314391078, -4.720307175313131, -3.0981438795672815]
+        self.tip1_loc = [0.049076405377552826, 0.35130426249264163, 0.063, 0.9759108742683295, -1.3350220046082053, -1.5092226077826993]
+        self.tip1_approach = [0.049076095756735466, 0.3513032390285145, 0.083, 0.9758916159413838, -1.3350252553821587, -1.5092057412143818]
+        self.tip1_above = [0.04908221782054774, 0.3513003341332178, 0.138, 0.9758574103691817, -1.3350463108315163, -1.5091909291569083]
+        self.tip2_loc = [0.04909177440821851, 0.3411316353820866, 0.0628, 0.977119433532159, -1.3337829736507698, -1.5108373189678133]
+        self.tip2_approach = [0.04909177440821851, 0.3411316353820866, 0.083, 0.977119433532159, -1.3337829736507698, -1.5108373189678133]
+        self.tip2_above = [0.04909177440821851, 0.3411316353820866, 0.138, 0.977119433532159, -1.3337829736507698, -1.5108373189678133]
+        self.sample1 = [0.15220619381604186, 0.21043816573205595, 0.09618091909170277, 1.444826407763332, -0.2548060433102738, -0.31289353129621067]
+        self.sample1_above = [0.15220723461648447, 0.2104311001071656, 0.14402782259610025, 1.4448359749910735, -0.2548206714588542, -0.31295915781137074]
+        self.sample2_above = [0.15279755520703087, 0.18939793717407497, 0.14402267332894347, 1.444821393022025, -0.25485812796155616, -0.3128929822914916]
+        self.sample2 = [0.15186061464767017, 0.18822197623964088, 0.09490910394912143, 1.4440966224799245, -0.255613147568461, -0.3122426586441542]
+        self.empty_tube = [0.15203368788019977, 0.16531582069324421, 0.12185568609417977, 1.4402850302548993, -0.2846256403901101, -0.3468228184833902]
+        self.empty_tube_above = [0.15203001904780783, 0.16531236663764431, 0.14222620538915642, 1.4402337440190125, -0.2846450307479814, -0.346876615018759]
+        self.well1 = [0.12772478460859046, 0.21370236710062357, 0.08390608100945282, 1.4380130231592743, -0.2414629895555231, -0.2954608172533908]
+        self.well1_above = [0.12773445855037924, 0.21371308008717516, 0.1271232135439438, 1.4380596200664426, -0.24151536289689018, -0.2954919320386042]
+        self.trash_bin_above = [0.187412530306272, 0.2868009561100828, 0.12712991727750073, 1.438076830279249, -0.2414934112798892, -0.2954944172453427]
+        self.trash_bin = [0.1874179391982658, 0.2867862635600429, 0.013156853887081085, 1.438022625162957, -0.24148065729851562, -0.2954808450568972]
 
     def connect_pipette(self):
         """
@@ -369,7 +476,7 @@ class PipetteController():
     
         print("Picking up the pipette...")
         sleep(1)
-        self.ur.movel(self.pipette_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.pipette_above,self.accel_mss,speed_ms,0,0)
         sleep(2)
         self.ur.movel(self.pipette_approach,self.accel_mss,speed_ms,0,0)
         speed_ms = 0.01
@@ -391,7 +498,6 @@ class PipetteController():
         print("Pipette successfully picked up")
        
 
-
     def pick_tip(self, x=0, y=0):
         """
         Description: Picks up a new tip from the first location on the pipette bin.
@@ -400,17 +506,17 @@ class PipetteController():
         print("Picking up the first pipette tip...")
         speed_ms = 0.100
 
-        self.ur.movel(self.tip1_above,self.ur.accel_radss,self.speed_rads,0,0)
+        self.ur.movel(self.tip1_above,self.accel_radss,self.speed_rads,0,0)
         sleep(2)
         speed_ms = 0.01
-        self.ur.movel(self.tip1_approach,self.ur.accel_radss,self.speed_rads,0,0)
+        self.ur.movel(self.tip1_approach,self.accel_radss,self.speed_rads,0,0)
         sleep(2)    
-        self.ur.movel(self.tip1_loc,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.tip1_loc,self.accel_mss,speed_ms,0,0)
         sleep(3)
-        self.ur.movel(self.tip1_approach,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.tip1_approach,self.accel_mss,speed_ms,0,0)
         sleep(2)
         speed_ms = 0.1
-        self.ur.movel(self.tip1_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.tip1_above,self.accel_mss,speed_ms,0,0)
         sleep(2)
         print("Pipette tip successfully picked up")
 
@@ -447,25 +553,25 @@ class PipetteController():
         
         # MOVE TO THE FIRT SAMPLE LOCATION
         speed_ms = 0.1
-        self.ur.movel(self.sample1_above,self.ur.accel_mss,self.speed_ms,0,0)
+        self.ur.movel(self.sample1_above,self.accel_mss,self.speed_ms,0,0)
         sleep(2)
-        self.ur.movel(self.sample1,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.sample1,self.accel_mss,speed_ms,0,0)
         sleep(2)
 
         # ASPIRATE FIRST SAMPLE
         self.aspirate_pipette()
-        self.ur.movel(self.sample1_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.sample1_above,self.accel_mss,speed_ms,0,0)
         sleep(1)
 
         # MOVE TO THE 1ST WELL
-        self.ur.movel(self.well1_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.well1_above,self.accel_mss,speed_ms,0,0)
         sleep(1)
-        self.ur.movel(self.well1,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.well1,self.accel_mss,speed_ms,0,0)
         sleep(1)
 
         # DISPENSE FIRST SAMPLE INTO FIRST WELL
         self.dispense_pipette()
-        self.ur.movel(self.well1_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.well1_above,self.accel_mss,speed_ms,0,0)
         sleep(1)
 
         # Changing tip
@@ -473,20 +579,20 @@ class PipetteController():
         self.pick_tip2()
 
         # MOVE TO THE SECON SAMPLE LOCATION
-        self.ur.movel(self.sample2_above,self.ur.accel_mss,self.speed_ms,0,0)
+        self.ur.movel(self.sample2_above,self.accel_mss,self.speed_ms,0,0)
         sleep(3)
-        self.ur.movel(self.sample2,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.sample2,self.accel_mss,speed_ms,0,0)
         sleep(2)
 
         # ASPIRATE SECOND SAMPLE
         self.aspirate_pipette()       
-        self.ur.movel(self.sample2_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.sample2_above,self.accel_mss,speed_ms,0,0)
         sleep(1)
 
         # MOVE TO THE 1ST WELL
-        self.ur.movel(self.well1_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.well1_above,self.accel_mss,speed_ms,0,0)
         sleep(1)    
-        self.ur.movel(self.well1,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.well1,self.accel_mss,speed_ms,0,0)
         sleep(1)
 
         # DISPENSE SECOND SAMPLE INTO FIRST WELL
@@ -500,7 +606,7 @@ class PipetteController():
         # Aspirate all the liquid   
         self.aspirate_pipette()
         self.aspirate_pipette()
-        self.ur.movel(self.well1_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.well1_above,self.accel_mss,speed_ms,0,0)
         sleep(1)
         print("Sample is prepared")
 
@@ -587,10 +693,10 @@ class PipetteController():
         print("Empting tip...")
         speed_ms = 0.5  
         # Moving the robot to the empty tube location
-        self.ur.movel(self.empty_tube_above,self.ur.accel_mss,self.speed_ms,0,0)
+        self.ur.movel(self.empty_tube_above,self.accel_mss,self.speed_ms,0,0)
         sleep(2)
         speed_ms = 0.1
-        self.ur.movel(self.empty_tube,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.empty_tube,self.accel_mss,speed_ms,0,0)
         sleep(2)
 
         # Drive the pipette three times to dispense all the liquid inside the pipette tip.
@@ -598,39 +704,9 @@ class PipetteController():
             self.dispense_pipette()
             sleep(1)
 
-        self.ur.movel(self.empty_tube_above,self.ur.accel_mss,speed_ms,0,0)
+        self.ur.movel(self.empty_tube_above,self.accel_mss,speed_ms,0,0)
         sleep(1)
     
-
-
-    def droplet_exp(self, tip_number_1:int = None, tip_number_2:int = None):
-        """
-        Description: Runs the full droplet experiment by calling the functions that perform each step in the experiment.
-        """
-        print("-*-*-* Starting the droplet experiment *-*-*-")
-        self.move_pipette_dock()
-        self.lock_tool_changer()
-        self.lift_pipette_on_dock()
-        self.home_robot()
-        self.pick_tip()
-        self.make_sample()
-        self.home_robot()
-        self.move_pipette_dock()
-        self.unlock_tool_changer()
-        self.lift_pipette_on_dock()        
-        self.create_droplet()
-        self.retrieve_droplet()
-        self.move_pipette_dock()
-        self.lock_tool_changer()
-        self.lift_pipette_on_dock()        
-        self.home_robot()
-        self.empty_tip()
-        self.drop_tip_to_trash()
-        self.home_robot()
-        self.move_pipette_dock()
-        self.unlock_tool_changer()
-        self.lift_pipette_on_dock()         
-        print("-*-*-* Droplet experiment is completed *-*-*-")
 
 
 class ToolChangerController():
