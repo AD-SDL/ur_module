a789d08063330f96b9f79b3a5aa8f04c9f15fdee
Dozgulbas <dozgulbas@anl.gov>
2023-08-02 14:32:05 -0500

    Camera controller needs organization

--- ur_driver/ur_driver/ur_tools/camera_controller.py
+++ ur_driver/ur_driver/ur_tools/camera_controller.py
@@ -1,6 +1,22 @@
 from time import sleep
 from copy import deepcopy
 
+import cv2
+import pyrealsense2 as rs
+import time
+import torch
+import numpy as np
+from torchvision.transforms import functional as F
+from ultralytics import YOLO
+import math
+from math import degrees
+from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper as gripper1
+import sys
+from robotiq_gripper_driver import RobotiqGripper
+# setting path
+from urx import Robot
+from transforms3d import euler, quaternions
+
 class CameraController():
 
     def __init__(self) -> None:
