299f1e4353059ee67d8134357eac4c414f738350
Dozgulbas <dozgulbas@anl.gov>
2023-08-03 14:39:01 -0500

    Working on the camera controller class

--- ur_driver/ur_driver/ur_tools/camera_controller.py
+++ ur_driver/ur_driver/ur_tools/camera_controller.py
@@ -2,22 +2,77 @@ from time import sleep
 from copy import deepcopy
 
 import cv2
-import pyrealsense2 as rs
-import time
+import pyrealsense2 as realsense
+
+
+from transforms3d import euler, quaternions
+from math import degrees
+
 import torch
-import numpy as np
 from torchvision.transforms import functional as F
 from ultralytics import YOLO
-import math
-from math import degrees
-from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper as gripper1
-import sys
+import numpy as np
+
 from robotiq_gripper_driver import RobotiqGripper
-# setting path
 from urx import Robot
-from transforms3d import euler, quaternions
 
 class CameraController():
 
-    def __init__(self) -> None:
-        pass
+
+    def __init__(self, ur_connection = None, target_object = None) -> None:
+        """This is a camera controller class created for the UR robots to utilize Intel Realsense D400 series cameres. 
+           Functionalities:  - Object detection based on a pre-trained YOLO model with 5 different labwares.
+                             - Plan a robot trajectory to pick up the objects based on distance and object reference frame imformation obtained from the camera.
+
+        :param [ur_connection]: [ParamDescription], defaults to [DefaultParamVal]
+        :type [ur_connection]: [class urx.Robot](connection to the urx.Robot)
+        ...
+        :raises [ErrorType]: [ErrorDescription]
+        ...
+        :return: [ReturnDescription]
+        :rtype: [ReturnType]
+        """
+        if not ur_connection:
+            raise Exception("ur connection is not provided!")
+        if not target_object:
+            raise Exception("Target object for YOLO model is not provided")
+        
+        self.pipeline = None
+        self.ur_connection =  ur_connection 
+
+        self.default_yolo_model_path = "best.pt"
+        self.model_object_list = ['deepwellplates', 'tipboxes', 'hammers', 'wellplates', 'wellplate_lids'] 
+        self.target_object = target_object
+        
+    def start_camera_stream(self) -> None:
+        """ Starts the Intel realsense camera pipeline 
+        :param:  None
+        :return: None
+        :return: None
+        """
+        self.pipeline = realsense.pipeline()
+        config = realsense.config()
+        config.enable_stream(realsense.stream.color, 640, 480, realsense.format.rgb8, 30)  # Color stream configuration
+        config.enable_stream(realsense.stream.depth, 640, 480, realsense.format.z16, 30)  # Depth stream configuration
+        profile = self.pipeline.start(config)
+
+    def capture_image(self) -> cv2:
+        # Capture a new image from the camera
+        frames = self.pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        img = np.asanyarray(color_frame.get_data())
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        return img
+    
+    def load_yolo_model(self, model_path = None):
+        if not model_path:
+            model_file_path = self.default_yolo_model_path
+        else:
+            model_file_path = model_path
+
+        # Load the trained YOLO model
+        model = YOLO(model_file_path)
+        # Set the desired objects to detect
+        desired_objects = ['deepwellplates'] #, 'tipboxes', 'hammers', 'deepwellplates', 'wellplate_lids']  #list of known objects
+        return model
