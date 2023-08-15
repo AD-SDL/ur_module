bcd75c7bc1039dea04c5d938ee6174931e3bf462
Dozgulbas <dozgulbas@anl.gov>
2023-08-15 15:20:38 -0500

    Working on the functions

--- ur_driver/ur_driver/ur_tools/wm_tool_changer_controller.py
+++ ur_driver/ur_driver/ur_tools/wm_tool_changer_controller.py
@@ -3,73 +3,83 @@ from copy import deepcopy
 
 import epics
 
-class ATIToolChangerController():
+class WMToolChangerController():
+    """Initilizes the WMToolChangerController to pick and place tools with the Wingman tool changers"""    
     
-
-    def __init__(self, tool_changer_pv:str = None):
-        self.current_tool = None
-        self.pv = tool_changer_pv
-        self.connect_tool_changer()
+    def __init__(self, tool_location:list = None, horizontal_axis:str = "y", ur_connection = None):
+        """
+        """
+        if not ur_connection:
+            raise AttributeError("Ur connection is not provided")
         
-    def connect_tool_changer(self):
-        """
-        Connect tool changer
-        """
+        self.robot = ur_connection
+        self.current_tool = None
+        self.axis = horizontal_axis
 
-        try:
-            # Establishing a connection with the tool changer on EPICS
-            self.tool_changer = epics.PV(self.pv)
+        self.tool_dock_l = [-0.30533163571362804, 0.293042569973924, 0.234306520730365, -3.1414391023029085, 0.014564845435757333, 0.0040377171549781125]
+        self.tool_dock_j = [2.692000389099121, -2.031496664086813, -1.3187065124511719, -1.3587687772563477, -4.709893051777975, 1.129366159439087]
+        self.tool_above_j = [2.691868543624878, -2.01027836422109, -1.1145529747009277, -1.584151407281393, -4.710240427647726, 1.1290664672851562]
+        self.tool_above_l = [-0.30521326850056485, 0.29304507597600077, 0.2842680699923231, -3.141487582034991, 0.014625666717814146, 0.0039104466707534005]
 
-        except Exception as err:
-            print("Tool changer error: ", err)
-
+        if tool_location:
+            self.location = tool_location
         else:
-            print("Tool changer is connected.")
+            self.location = self.tool_dock_l
 
-    def disconnect_tool_changer(self):
+        self.tool_above = self.location
+        self.tool_above[2] += 0.05
+        self.tool_front = None
+        self._get_tool_front()
+
+    def _get_tool_front(self):
         """
-        Disconnect tool changer
         """
+        self.tool_front = self.location
+        if self.axis == "x":
+            self.tool_front[0] += 0.1
+        elif self.axis == "-x":
+            self.tool_front[0] -= 0.1
+        elif self.axis == "y":
+            self.tool_front[1] += 0.1
+        elif self.axis == "-y":
+            self.tool_front[1] -= 0.1
 
-        try:
-            # Closing the connection with the tool changer on EPICS
-            self.tool_changer.disconnect()
+        #TODO: USe the tool location to find which quadrant will the robot be using (x,y) and then find the 6th joint rotation to figure out which direction the robot is looking at to extract the horizatal movement axis
 
-        except Exception as err:
-            print("Tool changer error: ", err)
-
-        else:
-            print("Tool changer is disconnected.")
-
-    def get_tool_changer_status(self):
+    def get_tool_status(self):
         """
         Description: 
             - Gets the tool changer current status. 
             - Tool changer is controlled by pyepics PV commands.
         """
-        status = self.tool_changer.get()
+        status = "STATUS"
         return status
 
-    def lock_tool_changer(self):
+    def pick_tool(self ):
         """
         Description: 
-            - Locks the tool changer. 
-            - Tool changer is controlled by pyepics PV commands.
+            Picks a new tool using the tool location.
         """
         try:
-            print("Locking the tool changer...")
-            self.tool_changer.put(1)
+            print("Picking up the tool...")
+            tool_above 
+            self.robot.movel(self.location)
         except Exception as err:
-            print("Error accured while locking the tool changer: ", err)
+            print("Error accured while picking up the tool changer: ", err)
 
-    def unlock_tool_changer(self):
+    def place_tool(self):
         """
         Description: 
-            - Unlocks the tool changer. 
-            - Tool changer is controlled by pyepics PV commands.
+            Places the currently attached tool back to the initial tool location
         """
         try:
-            print("Unlocking the tool changer...")
+            print("Placing the tool ...")
             self.tool_changer.put(0)
         except Exception as err:
             print("Error accured while unlocking the tool changer: ", err)
+
+    def discover_tool(self):
+        """
+        Discover if a tool is currently attached and which tool it is.
+        """
+        pass
