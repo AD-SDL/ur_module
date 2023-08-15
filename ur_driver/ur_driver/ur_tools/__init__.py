6ce789b2f5ba235525e9d00f897e274f79060c6c
Dozgulbas <dozgulbas@anl.gov>
2023-08-15 14:59:20 -0500

    Added new tool and pipette controllers

--- ur_driver/ur_driver/ur_tools/__init__.py
+++ ur_driver/ur_driver/ur_tools/__init__.py
@@ -1,8 +1,9 @@
 # __all__ = ["gripper_controller", "pipette_controller", "tool_changer_controller", "camera_controller"]
 
 from .gripper_controller import FingerGripperController, HandGripperController, VacuumGripperController
-from .epics_pipette_controller import EpicsPipetteController
-from .tool_changer_controller import ToolChangerController
+from .ot_pipette_controller import OTPipetteController
+from .aps_pipette_controller import ApsPipetteController
+from .ati_tool_changer_controller import ATIToolChangerController
 from .screwdriver_controller import ScrewdriverController
 from .camera_controller import CameraController
 from .urp_generator import URPGenerator
