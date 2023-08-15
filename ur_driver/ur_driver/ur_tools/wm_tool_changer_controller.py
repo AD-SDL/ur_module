c9fc51d524a9bf9455aa665cce82ee3e59ae53b0
Dozgulbas <dozgulbas@anl.gov>
2023-08-15 15:01:06 -0500

    Added Wingman tool changer controller

---
+++ ur_driver/ur_driver/ur_tools/wm_tool_changer_controller.py
@@ -0,0 +1,75 @@
+from time import sleep
+from copy import deepcopy
+
+import epics
+
+class ATIToolChangerController():
+    
+
+    def __init__(self, tool_changer_pv:str = None):
+        self.current_tool = None
+        self.pv = tool_changer_pv
+        self.connect_tool_changer()
+        
+    def connect_tool_changer(self):
+        """
+        Connect tool changer
+        """
+
+        try:
+            # Establishing a connection with the tool changer on EPICS
+            self.tool_changer = epics.PV(self.pv)
+
+        except Exception as err:
+            print("Tool changer error: ", err)
+
+        else:
+            print("Tool changer is connected.")
+
+    def disconnect_tool_changer(self):
+        """
+        Disconnect tool changer
+        """
+
+        try:
+            # Closing the connection with the tool changer on EPICS
+            self.tool_changer.disconnect()
+
+        except Exception as err:
+            print("Tool changer error: ", err)
+
+        else:
+            print("Tool changer is disconnected.")
+
+    def get_tool_changer_status(self):
+        """
+        Description: 
+            - Gets the tool changer current status. 
+            - Tool changer is controlled by pyepics PV commands.
+        """
+        status = self.tool_changer.get()
+        return status
+
+    def lock_tool_changer(self):
+        """
+        Description: 
+            - Locks the tool changer. 
+            - Tool changer is controlled by pyepics PV commands.
+        """
+        try:
+            print("Locking the tool changer...")
+            self.tool_changer.put(1)
+        except Exception as err:
+            print("Error accured while locking the tool changer: ", err)
+
+    def unlock_tool_changer(self):
+        """
+        Description: 
+            - Unlocks the tool changer. 
+            - Tool changer is controlled by pyepics PV commands.
+        """
+        try:
+            print("Unlocking the tool changer...")
+            self.tool_changer.put(0)
+        except Exception as err:
+            print("Error accured while unlocking the tool changer: ", err)
