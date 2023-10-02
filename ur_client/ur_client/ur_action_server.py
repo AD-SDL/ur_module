36cfd0322a15d936de355c5b330aabab4d5f6ab4
Dozgulbas <dozgulbas@anl.gov>
2023-10-02 16:18:29 -0500

    Working on the action server node

--- ur_client/ur_client/ur_action_server.py
+++ ur_client/ur_client/ur_action_server.py
@@ -1,33 +1,56 @@
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
-from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
 from std_msgs.msg import String
 
 from time import sleep
 import socket
 import json
 
-from ur_driver.ur_driver import UR 
 from ur_interfaces.action import RobotAction
 
-class UrActionServer(Node): # ACTION SERVER
+try:
+    from ur_driver.ur_driver import UR 
+except ImportError:
+    print("UR driver package cannot be used!")
+
+class UrActionServer(Node): #ACTION SERVER
 
     '''
-    The init function is neccesary for the URCLient class to initialize all variables, parameters, and other functions.
-    Inside the function the parameters exist, and calls to other functions and services are made so they can be executed in main.
+    This is a stakeholder for the UR action server class. 
     '''
    
-    def __init__(self, TEMP_NODE_NAME = "Ur_Node"):
+    def __init__(self, TEMP_NODE_NAME = "ur_action_server"):
 
         super().__init__(TEMP_NODE_NAME)
         self.node_name = self.get_name()
 
         self.ur = None
         self.IP = None
+        self.receive_launch_parameters()    
+
+        self.connect_robot()
+
+        self.state = "UNKNOWN"
+        self.robot_status = None
+        self.action_flag = "READY"
 
         self._action_server = ActionServer(self, RobotAction, self.node_name + '/robot_action', self.action_callback)
+   
+    def receive_launch_parameters(self):
+        
+        self.declare_parameter('ip', '146.137.240.38')       # Declaring parameter 
+        self.IP = self.get_parameter('ip').get_parameter_value().string_value     
+    
+    def connect_robot(self):
+        """Creates a connection with the robot over URx. This fuction utilizes the ur_driver package"""    
+        try:
+            self.ur = UR(self.IP)
+
+        except Exception as err:
+            self.get_logger().error(str(err))
+        else:
+            self.get_logger().info("ur connected")
 
     def action_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
