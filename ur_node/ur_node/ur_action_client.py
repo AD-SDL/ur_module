8fca98220c77f179cbf68ad8e952c7026b205653
Dozgulbas <dozgulbas@anl.gov>
2023-10-02 20:06:19 -0500

    Update

--- ur_node/ur_node/ur_action_client.py
+++ ur_node/ur_node/ur_action_client.py
@@ -10,7 +10,7 @@ import json
 
 class UrActionClient(Node): # ACTION CLIENT
 
-    def __init__(self, TEMP_NODE_NAME = "ur_node"):
+    def __init__(self, TEMP_NODE_NAME = "ur_node") -> None:
 
         super().__init__(TEMP_NODE_NAME)
         self.node_name = self.get_name()
@@ -20,19 +20,17 @@ class UrActionClient(Node): # ACTION CLIENT
         self.goal = None
         self._action_client = ActionClient(self, RobotAction, self.node_name + '/robot_action')
 
-    def send_goal(self, robot_goal):
+    def send_goal(self, robot_goal) -> None:
         self.goal = json.dumps(robot_goal)
         
         goal_msg = RobotAction.Goal()
         goal_msg.robot_goal = self.goal
         
         self._action_client.wait_for_server()
-    
         self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
-
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
-    def goal_response_callback(self, future):
+    def goal_response_callback(self, future) -> None:
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().warn('Goal rejected :(')
@@ -43,15 +41,14 @@ class UrActionClient(Node): # ACTION CLIENT
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
-    def get_result_callback(self, future):
+    def get_result_callback(self, future) -> None:
         result = future.result().result
         self.get_logger().info('Result: {0}'.format(result.robot_response))
         rclpy.shutdown()
 
-    def feedback_callback(self, feedback_msg):
+    def feedback_callback(self, feedback_msg) -> None:
         feedback = feedback_msg.feedback
         self.get_logger().info('Received feedback:' + feedback.robot_feedback)
-
 
 def main(args=None):
     rclpy.init(args=args)
