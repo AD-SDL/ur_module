dd9764e99d29bbf2c5c82628f8bae725365b302c
Dozgulbas <dozgulbas@anl.gov>
2023-10-06 16:08:42 -0500

    Working on the TCP socket for screwdriver

---
+++ ur_driver/ur_driver/ur_tools/robotiq_screwdriver_driver.py
@@ -0,0 +1,48 @@
+from pymodbus.client.sync import ModbusTcpClient
+
+class RobotiqScrewdriver:
+    def __init__(self, ip_address, port=502):
+        self.client = ModbusTcpClient(ip_address, port)
+        self.connection = False
+
+    def connect(self):
+        self.connection = self.client.connect()
+        if not self.connection:
+            raise ConnectionError("Failed to connect to the tool")
+
+    def disconnect(self):
+        self.client.close()
+
+    def set_torque(self, torque_value):
+        # assuming that torque value is set using a register, and register address is hypothetical
+        self.client.write_register(0x01, torque_value)
+
+    def set_rpm(self, rpm_value):
+        # assuming that RPM value is set using a register, and register address is hypothetical
+        self.client.write_register(0x02, rpm_value)
+
+    def activate_vacuum(self):
+        # assuming that activating vacuum is setting a coil to True, and coil address is hypothetical
+        self.client.write_coil(0x01, True)
+
+    def deactivate_vacuum(self):
+        self.client.write_coil(0x01, False)
+
+    def drive_forward(self):
+        # assuming that driving forward is setting a coil to True, and coil address is hypothetical
+        self.client.write_coil(0x02, True)
+
+    def drive_backward(self):
+        # assuming that driving backward is setting a coil to True, and coil address is hypothetical
+        self.client.write_coil(0x02, False)
+
+if __name__ == "__main__":
+    tool = RobotiqScrewdriver('192.168.1.10')  # replace with your device's IP
+    tool.connect()
+    tool.set_torque(100)
+    tool.set_rpm(200)
+    tool.activate_vacuum()
+    tool.drive_forward()
+    tool.deactivate_vacuum()
+    tool.drive_backward()
+    tool.disconnect()
