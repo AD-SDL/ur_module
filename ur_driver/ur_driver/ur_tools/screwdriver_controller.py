eef30ca8013323d063242e508747976ff7e9a839
Dozgulbas <dozgulbas@anl.gov>
2023-10-13 17:07:34 -0500

    Import drivers

--- ur_driver/ur_driver/ur_tools/screwdriver_controller.py
+++ ur_driver/ur_driver/ur_tools/screwdriver_controller.py
@@ -1,18 +1,25 @@
 from time import sleep
 from copy import deepcopy
-
+from .robotiq_screwdriver_driver import RobotiqScrewdriver
 class ScrewdriverController():
 
-    def __init__(self ):
+    def __init__(self, hostname:str = None, ur_connection = None ):
         """
         """
         #TODO: Make sure interpreter urp program exsists on the polyscope then start the program using the UR Dashboard.
         #TODO: Import screwdriver driver and handle all the motions as well as screwdriving jobs here
-            
+        self.hostname = hostname
+        if not ur_connection:
+            raise Exception("Failed to receive UR connection!")
+        else:
+            self.ur_connection == ur_connection    
         self.interpreter_urp = "/path/to/iterpreter.urp"
         self.hostanem = None
-        self.ur_connection = None
-        self.srewdriver = None
+        try:
+            self.srewdriver = RobotiqScrewdriver(hostname=self.hostname, socket_timeout=5)
+        except Exception as err:
+            print(err)
+            
     def check_screwdriver_controls(self):
         pass  
     def get_urp_programs(self):
