67b660e8fa44ca5b6776b89cd5a46434ab3366f2
Dozgulbas <dozgulbas@anl.gov>
2023-10-16 18:33:07 -0500

    Working on motions

--- ur_driver/ur_driver/ur_tools/screwdriver_controller.py
+++ ur_driver/ur_driver/ur_tools/screwdriver_controller.py
@@ -1,5 +1,6 @@
 from time import sleep
 from copy import deepcopy
+import numpy as np
 from .robotiq_screwdriver_driver import RobotiqScrewdriver
 class ScrewdriverController():
 
@@ -19,7 +20,7 @@ class ScrewdriverController():
             self.ur_connection == ur_connection    
         
         try:
-            self.srewdriver = RobotiqScrewdriver(hostname=self.hostname, socket_timeout=5)
+            self.screwdriver = RobotiqScrewdriver(hostname=self.hostname, socket_timeout=5)
         except Exception as err:
             print(err)
             
@@ -41,6 +42,75 @@ class ScrewdriverController():
             self.ur_connection.transfer_program(local_path = self.interpreter_urp, ur_path = iterpreter_program)
             response = self.ur_connection.load_program("iterpreter_program")
         self.ur_connection.run_program()
-    def
+
+    def pick_screw(self, screw_loc):
+        """
+        Description: Picks up a new screw.
+        """
+
+        screw_approach = deepcopy(screw_loc)
+        screw_approach[2] += 0.02
+        screw_above = deepcopy(screw_loc)
+        screw_above[2] += 0.15
+
+        print("Picking up the screw...")
+        speed_ms = 0.100
+
+        self.ur_connection.movel(screw_above,self.accel_radss,self.speed_rads)
+        sleep(2)
+        speed_ms = 0.01
+        self.screwdriver.activate_vacuum()
+        self.ur_connection.movel(screw_approach,self.accel_radss,self.speed_rads)
+        sleep(2)    
+        self.ur_connection.movel(screw_loc,self.accel_mss,speed_ms)
+        sleep(3)
+        self.ur_connection.movel(screw_approach,self.accel_mss,speed_ms)
+        sleep(2)
+        speed_ms = 0.1
+        self.ur_connection.movel(screw_above,self.accel_mss,speed_ms)
+        sleep(2)
+        if self.screwdriver.is_screw_detected() == "True":
+            print("Screw successfully picked up")
+        else:
+            print("Failed to pick the screw")
+
+    def screw_down(self, target):
+        """
+        Attempts to screws down the screw into the target location
+        """
+        target_approach = deepcopy(target)
+        target_approach[2] += 0.02
+        target_above = deepcopy(target)
+        z_height = 0.15
+        target_above[2] += z_height
+
+        print("Screwing down to the target...")
+        speed_ms = 0.100
+
+        self.ur_connection.movel(target_above,self.accel_radss,self.speed_rads)
+        sleep(2)
+        speed_ms = 0.01
+        self.ur_connection.movel(target_approach,self.accel_radss,self.speed_rads)
+        sleep(2)   
+
+        for i in np.arange(0,z_height,0.01):
+            self.screwdriver.drive_clockwise(angle=10)
+            self.ur_connection.translate_tool([0,0,i],0.1,0.1)
+        # TODO: OR try auto screw from the exact height from the surface of the object
+
+        # self.ur_connection.movel(target,self.accel_mss,speed_ms)
+        # sleep(3)
+        
+        self.screwdriver.deactivate_vacuum()
+        self.ur_connection.movel(target_approach,self.accel_mss,speed_ms)
+        sleep(2)
+        speed_ms = 0.1
+        self.ur_connection.movel(target_above,self.accel_mss,speed_ms)
+        sleep(2)
+        if self.screwdriver.is_screw_detected() == "True":
+            print("Screw successfully picked up")
+        else:
+            print("Failed to pick the screw")
+
 if __name__ == "__main__":
     screwdrive = ScrewdriverController(hostname="164.54.116.129")
