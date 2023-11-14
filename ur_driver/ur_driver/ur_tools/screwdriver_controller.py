13bcc6ba3ca8c43a94ab0cd5d2e8c2d449d138e5
Dozgulbas <dozgulbas@anl.gov>
2023-11-14 13:30:57 -0600

    Recieves only urx object

--- ur_driver/ur_driver/ur_tools/screwdriver_controller.py
+++ ur_driver/ur_driver/ur_tools/screwdriver_controller.py
@@ -4,11 +4,11 @@ from time import sleep
 from copy import deepcopy
 import numpy as np
 
-from .robotiq_screwdriver_driver import RobotiqScrewdriver
+from .uriq_screwdriver_driver import RobotiqScrewdriver
 
 class ScrewdriverController():
 
-    def __init__(self, hostname:str = None, ur = None):
+    def __init__(self, hostname:str = None, ur = None, ur_dashboard = None):
         """
         """
         #TODO: Make sure interpreter urp program exsists on the polyscope then start the program using the UR Dashboard.
@@ -26,9 +26,9 @@ class ScrewdriverController():
         if not ur:
             raise Exception("Failed to receive UR connection!")
         else:
-            self.ur = ur
-            self.robot = self.ur.ur_connection
-            self.robot.set_payload(3)
+            self.ur_dashboard = ur_dashboard
+            self.ur = ur.ur_connection
+            self.ur.set_payload(3)
         
         try:
             self.screwdriver = RobotiqScrewdriver(hostname=self.hostname, socket_timeout=5)
@@ -43,13 +43,12 @@ class ScrewdriverController():
         Makes sure that the interpreter socket is enabled on the robot PolyScope so that screwdriver commands can be sent over this socket.
         """
         iterpreter_program =  "/programs/interpreter_mode.urp"
-        response = self.ur.load_program(iterpreter_program)
+        response = self.ur_dashboard.load_program(iterpreter_program)
         if "File not found" in response:
-            self.ur.transfer_program(local_path = self.interpreter_urp, ur_path = iterpreter_program)
-            response = self.ur.load_program(iterpreter_program)
-        self.ur.run_program()
+            self.ur_dashboard.transfer_program(local_path = self.interpreter_urp, ur_path = iterpreter_program)
+            response = self.ur_dashboard.load_program(iterpreter_program)
+        self.ur_dashboard.run_program()
         sleep(2)
-        # Bug: Robot powers off after loading the program and needs a reboot. Check loaded program status and  make sure it works before moving to next steps
 
     def pick_screw(self, screw_loc:list = None, approach_axis:str = "z", approach_distance:float = 0.04):
         """
@@ -80,15 +79,15 @@ class ScrewdriverController():
 
         print("Picking up the screw...")
         
-        self.robot.movel(screw_above,1,1)
-        self.robot.movel(screw_approach,0.5,0.5)
-        self.robot.set_digital_out(self.air_switch_digital_output, True)
-        self.ur.run_program() #Restart interpreter program
+        self.ur.movel(screw_above,1,1)
+        self.ur.movel(screw_approach,0.5,0.5)
+        self.ur.set_digital_out(self.air_switch_digital_output, True)
+        self.ur_dashboard.run_program() #Restart interpreter program
         sleep(2)
         self.screwdriver.activate_vacuum()
         self.screwdriver.auto_screw()
         sleep(4)    
-        self.robot.movel(screw_above,1,0.5)
+        self.ur.movel(screw_above,1,0.5)
         sleep(2)
 
     def screw_down(self, target:list = None, approach_axis:str = "z", approach_distance:float = 0.02):
@@ -119,12 +118,12 @@ class ScrewdriverController():
 
         print("Screwing down to the target...")
         sleep(1)
-        self.robot.movel(target_above,1,1)
-        self.robot.set_digital_out(self.air_switch_digital_output, True)
+        self.ur.movel(target_above,1,1)
+        self.ur.set_digital_out(self.air_switch_digital_output, True)
 
-        self.robot.movel(target,1,1)
+        self.ur.movel(target,1,1)
         sleep(1)
-        self.ur.run_program() #Restart interpreter program
+        self.ur_dashboard.run_program() #Restart interpreter program
         sleep(2)
         self.screwdriver.activate_vacuum()
         self.screwdriver.auto_screw(250)
@@ -132,11 +131,11 @@ class ScrewdriverController():
         self.screwdriver.drive_clockwise(angle=200,rpm=100)
         sleep(2)
         self.screwdriver.deactivate_vacuum()
-        self.robot.set_digital_out(self.air_switch_digital_output, False)
+        self.ur.set_digital_out(self.air_switch_digital_output, False)
         sleep(1)
-        self.robot.movel(target_above,0.5,0.5)
+        self.ur.movel(target_above,0.5,0.5)
         sleep(2)
-        # self.ur.run_program() #Restart interpreter program
+        # self.ur_dashboard.run_program() #Restart interpreter program
         # sleep(2)
         print("Screw successfully placed")
 
