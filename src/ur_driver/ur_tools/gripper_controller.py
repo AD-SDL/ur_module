df958386262f8f16f0ea192211ac9c591bc6041f
Dozgulbas <dozgulbas@anl.gov>
2024-03-27 00:50:25 -0500

    Fixing docstrings

--- src/ur_driver/ur_tools/gripper_controller.py
+++ src/ur_driver/ur_tools/gripper_controller.py
@@ -1,12 +1,15 @@
-from time import sleep
+"""Controls Various Type of Gripper End Effectors"""
+
 from copy import deepcopy
+from time import sleep
 
 from .robotiq_gripper_driver import RobotiqGripper
 
-class FingerGripperController():
-    
 
-    def __init__(self, hostname:str = "146.137.240.38", port: int = 63352, ur = None):
+class FingerGripperController:
+    """Controls Robotiq Finger Grippers"""
+
+    def __init__(self, hostname: str = "146.137.240.38", port: int = 63352, ur=None):
         """
         Constructor for the FingerGripperController class.
 
@@ -16,26 +19,26 @@ class FingerGripperController():
         """
         self.host = hostname
         self.PORT = port
-        
+
         if not ur:
             raise Exception("UR connection is not established")
         else:
             self.ur = ur
             # self.ur.set_payload(1.2)# TODO: Check the actual payload
 
-        self.gripper_close = 130 # 0-255 (255 is closed)
+        self.gripper_close = 130  # 0-255 (255 is closed)
         self.gripper_open = 0
-        self.gripper_speed = 150 # 0-255
-        self.gripper_force = 50 # 0-255
+        self.gripper_speed = 150  # 0-255
+        self.gripper_force = 50  # 0-255
 
         self.acceleration = 0.7
         self.velocity = 0.7
-        self.speed_ms    = 0.750
-        self.speed_rads  = 0.750
-        self.accel_mss   = 1.200
+        self.speed_ms = 0.750
+        self.speed_rads = 0.750
+        self.accel_mss = 1.200
         self.accel_radss = 1.200
         self.blend_radius_m = 0.001
-        self.ref_frame = [0,0,0,0,0,0]
+        self.ref_frame = [0, 0, 0, 0, 0, 0]
 
     def connect_gripper(self):
         """
@@ -45,24 +48,26 @@ class FingerGripperController():
             try:
                 # GRIPPER SETUP:
                 self.gripper = RobotiqGripper()
-                print('Connecting to gripper...')
-                self.gripper.connect(hostname = self.host, port = self.PORT)
-                
+                print("Connecting to gripper...")
+                self.gripper.connect(hostname=self.host, port=self.PORT)
+
                 if self.gripper.is_active():
-                    print('Gripper already active')
+                    print("Gripper already active")
                 else:
-                    print('Activating gripper...')
+                    print("Activating gripper...")
                     self.gripper.activate()
-                    print('Opening gripper...')
+                    print("Opening gripper...")
                     self.open_gripper()
-            
+
             except Exception as err:
-                print("Gripper connection failed, try {}: {} ".format(i+1,err))
-                self.ur.set_tool_communication(baud_rate=115200,
-                                        parity=0,
-                                        stop_bits=1,
-                                        rx_idle_chars=1.5,
-                                        tx_idle_chars=3.5)       
+                print("Gripper connection failed, try {}: {} ".format(i + 1, err))
+                self.ur.set_tool_communication(
+                    baud_rate=115200,
+                    parity=0,
+                    stop_bits=1,
+                    rx_idle_chars=1.5,
+                    tx_idle_chars=3.5,
+                )
                 sleep(4)
 
             else:
@@ -80,16 +85,17 @@ class FingerGripperController():
         else:
             print("Gripper connection is closed")
 
-    def home_robot(self, home:list = None) -> None:
+    def home_robot(self, home: list = None) -> None:
         """
         Home the robot
         """
         if not home:
-            return 
+            return
         self.ur.movej(home, self.acceleration, self.velocity)
 
-        
-    def open_gripper(self, pose:float = None, speed:float = None, force:float = None) -> None:
+    def open_gripper(
+        self, pose: float = None, speed: float = None, force: float = None
+    ) -> None:
         """Opens the gripper using pose, speed and force variables"""
         if pose:
             self.gripper_open = pose
@@ -98,10 +104,14 @@ class FingerGripperController():
         if speed:
             self.gripper_speed = speed
 
-        self.gripper.move_and_wait_for_pos(self.gripper_open, self.gripper_speed, self.gripper_force)
+        self.gripper.move_and_wait_for_pos(
+            self.gripper_open, self.gripper_speed, self.gripper_force
+        )
         sleep(0.5)
-  
-    def close_gripper(self, pose:float = None, speed:float = None, force:float = None) -> None:
+
+    def close_gripper(
+        self, pose: float = None, speed: float = None, force: float = None
+    ) -> None:
         """Closes the gripper using pose, speed and force variables"""
         if pose:
             self.gripper_close = pose
@@ -110,16 +120,22 @@ class FingerGripperController():
         if speed:
             self.gripper_speed = speed
 
-        self.gripper.move_and_wait_for_pos(self.gripper_close, self.gripper_speed, self.gripper_force)
+        self.gripper.move_and_wait_for_pos(
+            self.gripper_close, self.gripper_speed, self.gripper_force
+        )
         sleep(0.5)
-        
-    def pick(self, pick_goal:list = None, approach_axis:str = None, approach_distance:float = None):
 
-        '''Pick up from first goal position'''
+    def pick(
+        self,
+        pick_goal: list = None,
+        approach_axis: str = None,
+        approach_distance: float = None,
+    ):
+        """Pick up from first goal position"""
 
         if not pick_goal:
             raise Exception("Please provide the source loaction")
-        
+
         if not approach_distance:
             approach_distance = 0.05
 
@@ -140,31 +156,34 @@ class FingerGripperController():
 
         above_goal = deepcopy(pick_goal)
         above_goal[axis] += approach_distance
-        
+
         self.open_gripper()
 
-        print('Moving to above goal position')
+        print("Moving to above goal position")
         self.ur.movel(above_goal, self.acceleration, self.velocity)
 
-        print('Moving to goal position')
+        print("Moving to goal position")
         self.ur.movel(pick_goal, self.acceleration, self.velocity)
 
-        print('Closing gripper')
+        print("Closing gripper")
         self.close_gripper()
 
-        print('Moving back to above goal position')
+        print("Moving back to above goal position")
         self.ur.movel(above_goal, self.acceleration, self.velocity)
 
-
-    def place(self, place_goal:list = None, approach_axis:str = None, approach_distance:float = None):
-
-        '''Place down at second goal position'''
+    def place(
+        self,
+        place_goal: list = None,
+        approach_axis: str = None,
+        approach_distance: float = None,
+    ):
+        """Place down at second goal position"""
         if not place_goal:
             raise Exception("Please provide the target loaction")
-        
+
         if not approach_distance:
             approach_distance = 0.05
-        
+
         axis = None
 
         if not approach_axis or approach_axis.lower() == "z":
@@ -183,31 +202,49 @@ class FingerGripperController():
         above_goal = deepcopy(place_goal)
         above_goal[axis] += approach_distance
 
-        print('Moving to above goal position')
+        print("Moving to above goal position")
         self.ur.movel(above_goal, self.acceleration, self.velocity)
 
-        print('Moving to goal position')
+        print("Moving to goal position")
         self.ur.movel(place_goal, self.acceleration, self.velocity)
 
-        print('Opennig gripper')
+        print("Opennig gripper")
         self.open_gripper()
 
-        print('Moving back to above goal position')
+        print("Moving back to above goal position")
         self.ur.movel(above_goal, self.acceleration, self.velocity)
 
-    def transfer(self, home:list = None, source:list = None, target:list = None, source_approach_axis:str = None, target_approach_axis:str = None, source_approach_distance: float = None, target_approach_distance: float = None) -> None:
+    def transfer(
+        self,
+        home: list = None,
+        source: list = None,
+        target: list = None,
+        source_approach_axis: str = None,
+        target_approach_axis: str = None,
+        source_approach_distance: float = None,
+        target_approach_distance: float = None,
+    ) -> None:
         """Handles the transfer request"""
-        self.pick(pick_goal = source, approach_axis = source_approach_axis, approach_distance = source_approach_distance)
+        self.pick(
+            pick_goal=source,
+            approach_axis=source_approach_axis,
+            approach_distance=source_approach_distance,
+        )
         print("Pick up completed")
         self.home_robot(home=home)
-        self.place(place_goal = target, approach_axis = target_approach_axis, approach_distance = target_approach_distance)
+        self.place(
+            place_goal=target,
+            approach_axis=target_approach_axis,
+            approach_distance=target_approach_distance,
+        )
         print("Place completed")
 
-class VacuumGripperController():
-    
 
-    def __init__(self, IP:str = "146.137.240.38", PORT: int = 29999, gripper:bool = False):
-        
+class VacuumGripperController:
+    """Robotiq Vacuum Gripper Controller"""
+
+    def __init__(
+        self, IP: str = "146.137.240.38", PORT: int = 29999, gripper: bool = False
+    ):
+        """Constructor for VacummGripperController"""
         super().__init__(IP=IP, PORT=PORT)
-
-
