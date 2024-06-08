bf90d41c6c79597d3e62c62f801fe3e73a703fb4
Dozgulbas <dozgulbas@anl.gov>
2024-06-07 17:59:59 -0700

    Clean up

--- src/ur_rest_node.py
+++ src/ur_rest_node.py
@@ -1,19 +1,122 @@
 """REST-based node for UR robots"""
 
+import datetime
+import traceback
+from typing import List
+
+from fastapi.datastructures import State
+from fastapi.responses import JSONResponse
+from typing_extensions import Annotated
+from ur_driver.ur import UR
 from wei.modules.rest_module import RESTModule
+from wei.types.module_types import ModuleStatus
+from wei.types.step_types import ActionRequest, StepResponse, StepStatus
 
 rest_module = RESTModule(
-    name="pf400_node",
+    name="ur_node",
     version="0.0.1",
-    description="A node to control the pf400 plate moving robot",
-    model="pf400",
+    description="A node to control the ur plate moving robot",
+    model="ur",
 )
 rest_module.arg_parser.add_argument(
-    "--pf400_ip", type=str, help="pf400 ip value", default="146.137.240.35"
+    "--ur_ip", type=str, help="IP address of the UR robot", default="164.54.116.129"
+)
+
+# Are the below arguments needed?
+rest_module.arg_parser.add_argument(
+    "--port", type=str, default="3011", help="Port for REST API"
 )
 rest_module.arg_parser.add_argument(
-    "--pf400_port", type=int, help="pf400 port value", default=10100
+    "--name", type=str, default="UR arms", help="Module name"
 )
+rest_module.arg_parser.add_argument(
+    "--host", type=str, default="0.0.0.0", help="Host IP/Domain Name"
+)
+
+
+@rest_module.startup()
+def ur_startup(state: State):
+    """UR startup handler."""
+    try:
+        state.ur = UR(state.ur_ip)
+        state.status = ModuleStatus.IDLE
+    except Exception:
+        state.status = ModuleStatus.ERROR
+        traceback.print_exc()
+    else:
+        print("UR online")
+    yield
+
+    # Do any cleanup here
+    pass
+
+
+def check_state(state: State):
+    """Gets robot status by checking robot dashboard status messages."""
+    state.ur.ur_dashboard.get_overall_robot_status()
+    if "NORMAL" not in state.ur.ur_dashboard.safety_status:
+        state.status = ModuleStatus.ERROR
+    elif state.ur.get_movement_state() == "BUSY":
+        state.status = ModuleStatus.BUSY
+    else:
+        state.status = ModuleStatus.IDLE
+
+
+@rest_module.state_handler()
+def state(state: State):
+    """Returns the current state of the UR module"""
+
+    if (
+        not (state.status == "BUSY")
+        or (state.status == "ERROR")
+        or (
+            state.action_start
+            and (
+                datetime.datetime.now() - state.action_start > datetime.timedelta(0, 2)
+            )
+        )
+    ):
+        check_state(state)
+    return JSONResponse(content={"State": state})
+
+
+@rest_module.action(
+    name="gripper_transfer",
+    description="Execute a transfer in between source and target locations using Robotiq grippers",
+)
+def gripper_transfer(
+    state: State,
+    action: ActionRequest,
+    home: Annotated[List[float], "Home location"],
+    source: Annotated[List[float], "Location to transfer sample from"],
+    target: Annotated[List[float], "Location to transfer sample to"],
+    source_approach_axis: Annotated[str, "Source location approach axis, (XYZ)"],
+    target_approach_axis: Annotated[str, "Source location approach axis, (XYZ)"],
+    source_approach_distance: Annotated[float, "Approach distance in meters"],
+    target_approach_distance: Annotated[float, "Approach distance in meters"],
+    gripper_open: Annotated[int, "Set a max value for the gripper open state"],
+    gripper_close: Annotated[int, "Set a min value for the gripper close state"],
+) -> StepResponse:
+    """Make a transfer using the finger gripper. This function uses linear motions to perform the pick and place movements."""
+
+    if not source or target or home:  # Return Fail
+        return StepResponse(
+            StepStatus.FAILED, "", "Source, target and home locations must be provided"
+        )
+
+    state.ur.gripper_transfer(
+        home=home,
+        source=source,
+        target=target,
+        source_approach_distance=source_approach_distance,
+        target_approach_distance=target_approach_distance,
+        source_approach_axis=source_approach_axis,
+        target_approach_axis=target_approach_axis,
+        gripper_open=gripper_open,
+        gripper_close=gripper_close,
+    )
+
+    return StepResponse(StepStatus.SUCCEEDED, "plate moved", None)
 
 
 rest_module.start()
