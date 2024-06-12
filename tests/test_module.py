d8c22344e768eea7f9ceaca96268f8ed283ca747
Dozgulbas <dozgulbas@anl.gov>
2024-06-12 12:05:29 -0700

    fixing unittests errors

--- tests/test_module.py
+++ tests/test_module.py
@@ -5,8 +5,7 @@ import unittest
 from pathlib import Path
 
 import requests
-
-# from wei.core.data_classes import ModuleAbout, Workcell
+from wei import ExperimentClient
 from wei.types import Workcell
 
 
@@ -23,7 +22,12 @@ class TestWEI_Base(unittest.TestCase):
         self.url = f"http://{self.server_host}:{self.server_port}"
         self.module_url = "http://ur_module:3011"
         self.redis_host = self.workcell.config.redis_host
-
+        self.experiment = ExperimentClient(
+            server_host=self.server_host,
+            server_port=self.server_port,
+            experiment_name="Test_Experiment",
+            working_dir=Path(__file__).resolve().parent,
+        )
         # Check to see that server is up
         start_time = time.time()
         while True:
@@ -35,26 +39,8 @@ class TestWEI_Base(unittest.TestCase):
             time.sleep(1)
             if time.time() - start_time > 60:
                 raise TimeoutError("Server did not start in 60 seconds")
-        while True:
-            try:
-                if requests.get(self.module_url + "/state").status_code == 200:
-                    break
-            except Exception:
-                pass
-            time.sleep(1)
-            if time.time() - start_time > 60:
-                raise TimeoutError("Module did not start in 60 seconds")
-
-
-# class TestModuleInterfaces(TestWEI_Base):
-#     """Tests the basic functionality of the Module."""
-
-#     def test_module_about(self):
-#         """Tests that the module's /about endpoint works"""
-#         response = requests.get(self.module_url + "/about")
-#         assert response.status_code == 200
-#         ModuleAbout(**response.json())
 
 
 if __name__ == "__main__":
+    t = TestWEI_Base()
     unittest.main()
