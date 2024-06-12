356497cc0cd876112cf52ff5d0e1665228f02a89
Dozgulbas <dozgulbas@anl.gov>
2024-06-11 18:47:14 -0700

    Working on replacing the WEI data classes

--- tests/test_module.py
+++ tests/test_module.py
@@ -17,8 +17,7 @@ class TestWEI_Base(unittest.TestCase):
         """Basic setup for WEI's pytest tests"""
         super().__init__(*args, **kwargs)
         self.root_dir = Path(__file__).resolve().parent.parent
-        self.workcell_file = self.root_dir / Path("tests/workcell_defs/test_workcell.yaml")
-        self.workcell = Workcell.from_yaml(self.workcell_file)
+        self.workcell = Workcell.from_yaml(self.root_dir / Path("tests/workcells/test_workcell.yaml"))
         self.server_host = self.workcell.config.server_host
         self.server_port = self.workcell.config.server_port
         self.url = f"http://{self.server_host}:{self.server_port}"
@@ -47,14 +46,14 @@ class TestWEI_Base(unittest.TestCase):
                 raise TimeoutError("Module did not start in 60 seconds")
 
 
-class TestModuleInterfaces(TestWEI_Base):
-    """Tests the basic functionality of the Module."""
+# class TestModuleInterfaces(TestWEI_Base):
+#     """Tests the basic functionality of the Module."""
 
-    def test_module_about(self):
-        """Tests that the module's /about endpoint works"""
-        response = requests.get(self.module_url + "/about")
-        assert response.status_code == 200
-        # ModuleAbout(**response.json())
+#     def test_module_about(self):
+#         """Tests that the module's /about endpoint works"""
+#         response = requests.get(self.module_url + "/about")
+#         assert response.status_code == 200
+#         ModuleAbout(**response.json())
 
 
 if __name__ == "__main__":
