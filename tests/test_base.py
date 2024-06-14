3b2993ab405824245d13418d16fbdd4c687cdfa4
Dozgulbas <dozgulbas@anl.gov>
2024-06-13 17:04:49 -0700

    IP updated

--- tests/test_base.py
+++ tests/test_base.py
@@ -13,7 +13,7 @@ class TestModule_Base(unittest.TestCase):
 class TestImports(TestModule_Base):
     """Test the imports of the module are working correctly"""
 
-    @patch("sys.argv", ["test", "--ur_ip", "127.0.0.1"])
+    @patch("sys.argv", ["test", "--ur_ip", "164.54.116.129"])
     def test_driver_import(self):
         """Test the driver and rest node imports"""
         import ur_driver
