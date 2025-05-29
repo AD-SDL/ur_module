8cf0f7cb6b27dbcc42fc91e51c8f2c116dd915f4
Dozgulbas <dozgulbas@anl.gov>
2025-05-29 15:59:10 -0500

    Added two new functions

--- src/ur_interface/ur.py
+++ src/ur_interface/ur.py
@@ -661,6 +661,79 @@ class UR:
         except Exception as err:
             print(err)
 
+    def pipette_pick_and_move_sample(
+        self,
+        home: Union[LocationArgument, list] = None,
+        tip_loc: Union[LocationArgument, list] = None,
+        sample_loc: Union[LocationArgument, list] = None,
+        target: Union[LocationArgument, list] = None,
+        volume: int = 10,
+    ) -> None:
+        """Pipette pick sample from the source location and transfer it to the target location
+
+        Args
+            home (Union[LocationArgument, list]): Home location
+            tip_loc (Union[LocationArgument, list]): Pipette tip location
+            sample_loc (Union[LocationArgument, list]): Sample location
+            target (Union[LocationArgument, list]): Target location
+            volume (int): Pipette transfer volume. Unit number of steps. Each step is 1 mL
+        """
+        if not sample_loc or not target:
+            raise Exception("Please provide both the sample and target loactions to make a transfer")
+
+        try:
+            pipette = TricontinentPipetteController(
+                hostname=self.hostname,
+                ur=self.ur_connection,
+                pipette_ip=self.hostname,
+                resource_client=self.resource_client,
+                pipette_resource_id=self.tool_resource_id,
+            )
+            pipette.connect_pipette()
+            if tip_loc:
+                pipette.pick_tip(tip_loc=tip_loc)
+            self.home(home)
+            pipette.pick_and_move(home=home, sample_loc=sample_loc, target=target, vol=volume)
+            pipette.disconnect_pipette()
+            print("Disconnecting from the pipette")
+        except Exception as err:
+            print(err)
+
+    def pipette_dispense_and_retrieve(
+        self,
+        home: Union[LocationArgument, list] = None,
+        tip_trash: Union[LocationArgument, list] = None,
+        target: Union[LocationArgument, list] = None,
+        volume: int = 10,
+    ) -> None:
+        """Dispense a sample using the pipette. This function uses linear motions to perform the pick and place movements.
+        Args
+            home (Union[LocationArgument, list]): Home location joint values
+            tip_trash (Union[LocationArgument, list]): Tip trash location
+            target (Union[LocationArgument, list]): Target location
+            volume (int): Pipette transfer volume. Unit number of steps. Each step is 1 mL
+        """
+        if not target:
+            raise Exception("Please provide the target loaction to make a dispense")
+
+        try:
+            pipette = TricontinentPipetteController(
+                hostname=self.hostname,
+                ur=self.ur_connection,
+                pipette_ip=self.hostname,
+                resource_client=self.resource_client,
+                pipette_resource_id=self.tool_resource_id,
+            )
+            pipette.connect_pipette()
+            pipette.dispense_and_retrieve(home=home, target=target, vol=volume)
+            if tip_trash:
+                pipette.eject_tip(eject_tip_loc=tip_trash, approach_axis="y")
+            pipette.disconnect_pipette()
+            self.home(home)
+            print("Disconnecting from the pipette")
+        except Exception as err:
+            print(err)
+
     def run_droplet(
         self,
         home: Union[LocationArgument, list] = None,
